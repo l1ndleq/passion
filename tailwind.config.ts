4967a6b1cfd6b46ce5d33189f9d4552fc1d3a073
l1ndleq <wideolegyt@gmail.com>
2026-02-04 21:53:47 +0300

    Fix: include components in tailwind content and prevent image stretching

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,12 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+ content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}", // ✅ ДОБАВЬ ЭТО
+    "./lib/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: { extend: {} },
   plugins: [],
 };
